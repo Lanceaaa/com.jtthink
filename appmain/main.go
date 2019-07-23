e9463b5c8dd65eba7f3ad8287c02a0b52cf03e6e
name <email>
2019-07-23 19:27:30 +0800

    study

--- appmain/main.go
+++ appmain/main.go
@@ -5,9 +5,11 @@ import (
     _ "com.jtthink/services"
 	// . "com.jtthink/core"
 	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
-	"os"
+	// "database/sql"
+	// "os"
 	_ "com.jtthink/models"
+	// "time"
+	// "strings"
 )
 
 type all interface {
@@ -20,46 +22,88 @@ func test(str ...string) {
 	}
 }
 
+func sum(min, max int, c chan int)  {
+	result := 0
+	for i := min; i <= max; i++ {
+		result = result + i
+	}
+	c <- result
+}
+
 func main()  {
 	// fmt.Println(GetService().Get(123))
 
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang?charset=utf8mb4")
-	if err != nil {
-		fmt.Println("Failed to connect to mysql, err:"+ err.Error())
-		os.Exit(1)
-	}
+	// num := 100000000
+	// start := time.Now()
+	// c := make(chan int, 2)
+	// go sum(1, num/2, c)
+	// go sum(num/2+1, num, c)
+	// ret1, ret2 := <- c, <- c
 
-	rows, err := db.Query("select user_name, signup_at from tbl_user")
-	if err != nil {
-		fmt.Println("Failed to select to mysql, err:"+ err.Error())
-		return
-	}
+	// users := strings.Split("shenyi,zhangsan,lisi,wangwu", ",")
+	// ages := strings.Split("17,67,5,8,6", ",")
+	
+	// c1, c2 := make(chan bool), make(chan bool)
+	// ret := make([]string, 0)
 
-	columns, _ := rows.Columns()
+	// go func ()  {
+	// 	for _, v := range users {
+	// 		<- c1 
+	// 		ret = append(ret, v)
+	// 		c2 <- true
+	// 	}
+	// } ()
+	// go func ()  {
+	// 	for _, v := range ages {
+	// 		<- c2
+	// 		ret = append(ret, v)
+	// 		c1 <- true
+	// 	}
+	// } ()
+	// c1 <- true
+	// fmt.Println(ret)
 
-	allRows := make([]interface{}, 0)
-	for rows.Next() {
-	    fileMap := make(map[string]interface{})
-		row := make([]interface{}, len(columns))
-		scanRow := make([]interface{}, len(columns))
-		for i, _ := range row {
-			scanRow[i] = &row[i]
-		}
-		// rows.Scan(&row[0], &row[1])
-		rows.Scan(scanRow...)
 
-		for k, val := range row {
-			v, ok := val.([]byte)
-			if ok {
-				// row[k] = string(v)
-				fileMap[columns[k]] = string(v)
-			}
-		}
+	// end := time.Now()
+	// fmt.Println(end.Sub(start), ret1 + ret2)
 
-		allRows = append(allRows, fileMap)
-	}
+	// db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang?charset=utf8mb4")
+	// if err != nil {
+	// 	fmt.Println("Failed to connect to mysql, err:"+ err.Error())
+	// 	os.Exit(1)
+	// }
 
-	for _, v := range allRows {
-		fmt.Println(v)
-	}
+	// rows, err := db.Query("select user_name, signup_at from tbl_user")
+	// if err != nil {
+	// 	fmt.Println("Failed to select to mysql, err:"+ err.Error())
+	// 	return
+	// }
+
+	// columns, _ := rows.Columns()
+
+	// allRows := make([]interface{}, 0)
+	// for rows.Next() {
+	//     fileMap := make(map[string]interface{})
+	// 	row := make([]interface{}, len(columns))
+	// 	scanRow := make([]interface{}, len(columns))
+	// 	for i, _ := range row {
+	// 		scanRow[i] = &row[i]
+	// 	}
+	// 	// rows.Scan(&row[0], &row[1])
+	// 	rows.Scan(scanRow...)
+
+	// 	for k, val := range row {
+	// 		v, ok := val.([]byte)
+	// 		if ok {
+	// 			// row[k] = string(v)
+	// 			fileMap[columns[k]] = string(v)
+	// 		}
+	// 	}
+
+	// 	allRows = append(allRows, fileMap)
+	// }
+
+	// for _, v := range allRows {
+	// 	fmt.Println(v)
+	// }
 }
